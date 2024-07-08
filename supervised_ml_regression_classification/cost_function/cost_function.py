fd7868c79d7087c23270318254a4981551b6b1aa
Michael Lazar <mikelazar51@gmail.com>
2024-07-08 17:37:02 -0500

    second lab, added jupyter notebook

---
+++ supervised_ml_regression_classification/cost_function/cost_function.py
@@ -0,0 +1,35 @@
+import numpy as np
+# %matplotlib widget
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
+
+x_train = np.array([1.0, 2.0])           #(size in 1000 square feet)
+y_train = np.array([300.0, 500.0])           #(price in 1000s of dollars)
+
+plt_intuition(x_train,y_train)
